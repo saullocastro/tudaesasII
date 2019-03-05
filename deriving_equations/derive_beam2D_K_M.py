5137e09fbe9e9432c0f2da354049ed378c864025
saullocastro <castrosaullo@gmail.com>
2019-03-05 11:48:12 +0100

    MAINT: more printing

--- deriving_equations/derive_beam2D_K_M.py
+++ deriving_equations/derive_beam2D_K_M.py
@@ -14,9 +14,8 @@ R = sympy.Matrix([[  cosr,  sinr , 0, 0, 0, 0],
                   [ 0, 0, 0, -sinr,  cosr , 0],
                   [ 0, 0, 0, 0,  0 , 1]])
 
-#for leg_poly in [False, True]:
 ONE = sympy.Integer(1)
-for leg_poly in [True, False]:
+for leg_poly in [True]:
     if leg_poly:
         Nv1 = ONE/2 - 3*xi/4 + 1*xi**3/4
         Nv2 = le*(ONE/8 - 1*xi/8 - 1*xi**2/8 + 1*xi**3/8)
@@ -40,17 +39,31 @@ for leg_poly in [True, False]:
     Ke[:, :] = (2/le)*E*Izz*Nbetaxi.T*Nbetaxi + (2/le)*E*A*Nuxi.T*Nuxi
     Me[:, :] = (le/2)*rho*(A*Nu.T*Nu + A*Nv.T*Nv + Izz*Nbeta.T*Nbeta)
 
+    # integrating matrices in natural coordinates
+
     for ind, val in np.ndenumerate(Ke):
         Ke[ind] = sympy.simplify(sympy.integrate(val, (xi, -1, 1)))
 
     for ind, val in np.ndenumerate(Me):
         Me[ind] = sympy.simplify(sympy.integrate(val, (xi, -1, 1)))
 
+    K = sympy.simplify(R.T*Ke*R)
+
     if leg_poly:
         print('K integrated with Legendre polynomial')
     else:
         print('K integrated with Hermitian cubic polynomial')
-    K = R.T*Ke*R
+
+    print('printing for assignment report')
+    for ind, val in np.ndenumerate(Ke):
+        i, j = ind
+        if val == 0:
+            continue
+        print('K_e[%d, %d] =' % (i+1, j+1), end='')
+        sympy.print_latex(Ke[ind].subs('le', 'l_e'))
+        print(r'\\')
+
+    print('printing for code')
     for ind, val in np.ndenumerate(K):
         i, j = ind
         si = 'c1' if i < 3 else 'c2'
@@ -61,7 +74,19 @@ for leg_poly in [True, False]:
         print('M integrated with Legendre polynomial')
     else:
         print('M integrated with Hermitian polynomial')
-    M = R.T*Me*R
+
+    M = sympy.simplify(R.T*Me*R)
+
+    print('printing for assignment report')
+    for ind, val in np.ndenumerate(Me):
+        i, j = ind
+        if val == 0:
+            continue
+        print('M_e[%d, %d] =' % (i+1, j+1), end='')
+        sympy.print_latex(Me[ind].subs('le', 'l_e'))
+        print(r'\\')
+
+    print('printing for code')
     for ind, val in np.ndenumerate(M):
         i, j = ind
         si = 'c1' if i < 3 else 'c2'
