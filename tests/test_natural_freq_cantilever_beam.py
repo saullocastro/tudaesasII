fb86a2a9bcb4bf1ada81b02acbf5fa5fae105a55
saullocastro <saullogiovani@gmail.com>
2020-01-08 12:18:38 +0100

    ENH: using standard SciPy libraries

--- tests/test_natural_freq_cantilever_beam.py
+++ tests/test_natural_freq_cantilever_beam.py
@@ -1,9 +1,11 @@
 import numpy as np
-from structsolve import freq
+from scipy.linalg import eigh
 
 from tudaesasII.beam2D import Beam2D, update_K_M
 
-def test_nat_freq_cantilever_beam(plot=False):
+DOF = 3 # DOF per node
+
+def test_nat_freq_cantilever_beam(plot=False, mode=0):
     n = 100
 
     L = 3 # total size of the beam along x
@@ -50,16 +52,28 @@ def test_nat_freq_cantilever_beam(plot=False):
         beams.append(beam)
 
     # applying boundary conditions
+    bk = np.zeros(K.shape[0], dtype=bool) #array to store known DOFs
+    check = np.isclose(x, 0.) # locating node at root
     # clamping at root
-    K[0:3, :] = 0
-    K[:, 0:3] = 0
-    M[0:3, :] = 0
-    M[:, 0:3] = 0
+    for i in range(DOF):
+        bk[i::DOF] = check
+    bu = ~bk # same as np.logical_not, defining unknown DOFs
 
-    eigvals, eigmodes = freq(K, M, sparse_solver=True)
-    omegan = ((-eigvals)**0.5).real
+    # sub-matrices corresponding to unknown DOFs
+    Kuu = K[bu, :][:, bu]
+    Muu = M[bu, :][:, bu]
+
+    eigvals, U = eigh(a=Kuu, b=Muu)
+    omegan = eigvals**0.5
+
+    # vector u containing displacements for all DOFs
+    u = np.zeros(K.shape[0], dtype=float)
+    u[bu] = U[:, mode]
+
+    # reference answer
     alpha123 = np.array([1.875, 4.694, 7.885])
     omega123 = alpha123**2*np.sqrt(E*Izz/(rho*A*L**4))
+
     print('Theoretical omega123', omega123)
     print('Numerical omega123', omegan[0:3])
     assert np.allclose(omega123, omegan[0:3], rtol=0.01)
@@ -71,8 +85,8 @@ def test_nat_freq_cantilever_beam(plot=False):
         import matplotlib.pyplot as plt
 
         plt.plot(x, y, '-k')
-        plt.plot(x+u[:, 0], y+u[:, 1], '--r')
+        plt.plot(x+u[0::DOF], y+u[1::DOF], '--r')
         plt.show()
 
 if __name__ == '__main__':
-    test_nat_freq_cantilever_beam(plot=True)
+    test_nat_freq_cantilever_beam(plot=True, mode=0)
