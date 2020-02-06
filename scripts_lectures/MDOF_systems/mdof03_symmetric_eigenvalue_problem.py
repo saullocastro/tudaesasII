d3d470d442d8336dd6a3af1555478e73ec3cbf34
saullocastro <saullogiovani@gmail.com>
2020-02-06 13:35:16 +0100

    updated examples

--- scripts_lectures/MDOF_systems/mdof03_symmetric_eigenvalue_problem.py
+++ scripts_lectures/MDOF_systems/mdof03_symmetric_eigenvalue_problem.py
@@ -1,73 +1,60 @@
+import sys
+sys.path.append('../..')
+
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from scipy.spatial import Delaunay
 from scipy.linalg import eigh, cholesky
 
-from truss2d import Truss2D, update_K_M
+from tudaesasII.beam2d import Beam2D, update_K, update_M, DOF
 
-DOF = 2
-
-lumped = False
-
-# number of nodes in each direction
-nx = 20
-ny = 4
+# number of nodes along x
+nx = 100
 
 # geometry
-a = 10
-b = 1
-A = 0.01**2
+L = 10
+h = 1
+w = h/10
+Izz = h**3*w/12
+A = w*h
 
 # material properties
 E = 70e9
+nu = 0.33
 rho = 2.6e3
 
 # creating mesh
-xtmp = np.linspace(0, a, nx)
-ytmp = np.linspace(0, b, ny)
-xmesh, ymesh = np.meshgrid(xtmp, ytmp)
+xmesh = np.linspace(0, L, nx)
+ymesh = np.zeros_like(xmesh)
 ncoords = np.vstack((xmesh.T.flatten(), ymesh.T.flatten())).T
 x = ncoords[:, 0]
 y = ncoords[:, 1]
 nid_pos = dict(zip(np.arange(len(ncoords)), np.arange(len(ncoords))))
 
-# triangulation to establish nodal connectivity
-d = Delaunay(ncoords)
-
-# extracting edges out of triangulation to form the truss elements
-edges = {}
-for s in d.simplices:
-    edges[tuple(sorted([s[0], s[1]]))] = [s[0], s[1]]
-    edges[tuple(sorted([s[1], s[2]]))] = [s[1], s[2]]
-    edges[tuple(sorted([s[2], s[0]]))] = [s[2], s[0]]
-nAnBs = np.array([list(edge) for edge in edges.values()], dtype=int)
-
 #NOTE using dense matrices
-K = np.zeros((DOF*nx*ny, DOF*nx*ny))
-M = np.zeros((DOF*nx*ny, DOF*nx*ny))
+K = np.zeros((DOF*nx, DOF*nx))
+M = np.zeros((DOF*nx, DOF*nx))
 
-# creating truss elements
 elems = []
-for n1, n2 in nAnBs:
-    elem = Truss2D()
+# creating beam elements
+nids = list(nid_pos.keys())
+for n1, n2 in zip(nids[:-1], nids[1:]):
+    elem = Beam2D()
     elem.n1 = n1
     elem.n2 = n2
     elem.E = E
-    elem.A = A
+    elem.A1 = elem.A2 = A
+    elem.Izz1 = elem.Izz2 = Izz
     elem.rho = rho
-    update_K_M(elem, nid_pos, ncoords, K, M, lumped=lumped)
+    elem.interpolation = 'legendre'
+    update_K(elem, nid_pos, ncoords, K)
+    update_M(elem, nid_pos, M)
     elems.append(elem)
-if lumped:
-    assert np.count_nonzero(M-np.diag(np.diagonal(M))) == 0
 
 # applying boundary conditions
 bk = np.zeros(K.shape[0], dtype=bool) # defining known DOFs
 check = np.isclose(x, 0.)
 bk[0::DOF] = check
 bk[1::DOF] = check
+bk[2::DOF] = check
 bu = ~bk # defining unknown DOFs
 
 # sub-matrices corresponding to unknown DOFs
@@ -90,7 +77,6 @@ assert np.allclose(Kuutilde, Kuutilde.T)
 eigvals_s, V = eigh(Kuutilde)
 wn_s = eigvals_s**0.5
 
-
 print('eigenvalues (wn_generalized**2)', wn_g[:3]**2)
 print('eigenvalues (wn_symmetric**2)  ', wn_s[:3]**2)
 print()
@@ -104,5 +90,6 @@ print()
 print('checks for V')
 for I, J in [[0, 0], [0, 1], [0, 2], [1, 1], [1, 2], [2, 2]]:
     print('I =', I, 'J =', J,
-          '\tVI . VJ %1.2f' % (V[:, I] @ V[:, J]))
+          '\tVI . VJ %1.2f' % (V[:, I] @ V[:, J]),
+          '\tVI K_tilde VJ %1.2f' % (V[:, I] @ Kuutilde @ V[:, J]))
 
--- scripts_lectures/MDOF_systems/mdof04_free_undamped_vibration.py
+++ scripts_lectures/MDOF_systems/mdof04_free_undamped_vibration.py
@@ -1,73 +1,63 @@
-import numpy as np
+import sys
+sys.path.append('../..')
+
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from scipy.spatial import Delaunay
-from scipy.linalg import cholesky, eigh
+import numpy as np
+from scipy.linalg import eigh, cholesky
 
-from truss2d import Truss2D, update_K_M
+from tudaesasII.beam2d import Beam2D, update_K, update_M, DOF
 
-DOF = 2
-
-lumped = False
-
-# number of nodes in each direction
-nx = 20
-ny = 4
+# number of nodes along x
+nx = 10
 
 # geometry
-a = 10
-b = 1
-A = 0.01**2
+length = 10
+h = 1
+w = h/10
+Izz = h**3*w/12
+A = w*h
 
 # material properties
 E = 70e9
+nu = 0.33
 rho = 2.6e3
 
 # creating mesh
-xtmp = np.linspace(0, a, nx)
-ytmp = np.linspace(0, b, ny)
-xmesh, ymesh = np.meshgrid(xtmp, ytmp)
+xmesh = np.linspace(0, length, nx)
+ymesh = np.zeros_like(xmesh)
 ncoords = np.vstack((xmesh.T.flatten(), ymesh.T.flatten())).T
 x = ncoords[:, 0]
 y = ncoords[:, 1]
 nid_pos = dict(zip(np.arange(len(ncoords)), np.arange(len(ncoords))))
 
-# triangulation to establish nodal connectivity
-d = Delaunay(ncoords)
-
-# extracting edges out of triangulation to form the truss elements
-edges = {}
-for s in d.simplices:
-    edges[tuple(sorted([s[0], s[1]]))] = [s[0], s[1]]
-    edges[tuple(sorted([s[1], s[2]]))] = [s[1], s[2]]
-    edges[tuple(sorted([s[2], s[0]]))] = [s[2], s[0]]
-nAnBs = np.array([list(edge) for edge in edges.values()], dtype=int)
-
 #NOTE using dense matrices
-K = np.zeros((DOF*nx*ny, DOF*nx*ny))
-M = np.zeros((DOF*nx*ny, DOF*nx*ny))
+K = np.zeros((DOF*nx, DOF*nx))
+M = np.zeros((DOF*nx, DOF*nx))
 
-# creating truss elements
 elems = []
-for n1, n2 in nAnBs:
-    elem = Truss2D()
+# creating beam elements
+nids = list(nid_pos.keys())
+for n1, n2 in zip(nids[:-1], nids[1:]):
+    elem = Beam2D()
     elem.n1 = n1
     elem.n2 = n2
     elem.E = E
-    elem.A = A
+    elem.A1 = elem.A2 = A
+    elem.Izz1 = elem.Izz2 = Izz
     elem.rho = rho
-    update_K_M(elem, nid_pos, ncoords, K, M, lumped=lumped)
+    elem.interpolation = 'legendre'
+    update_K(elem, nid_pos, ncoords, K)
+    update_M(elem, nid_pos, M)
     elems.append(elem)
-if lumped:
-    assert np.count_nonzero(M-np.diag(np.diagonal(M))) == 0
 
 # applying boundary conditions
 bk = np.zeros(K.shape[0], dtype=bool) # defining known DOFs
 check = np.isclose(x, 0.)
 bk[0::DOF] = check
 bk[1::DOF] = check
+bk[2::DOF] = check
 bu = ~bk # defining unknown DOFs
 
 # sub-matrices corresponding to unknown DOFs
@@ -82,12 +72,11 @@ Kuutilde = (Linv @ Kuu) @ Linv.T
 eigvals, V = eigh(Kuutilde)
 wn = eigvals**0.5
 
-
 u0 = np.zeros(K.shape[0])
 # linear
-u0[1::DOF] = 0.5*(ncoords[:, 0]/a)
+u0[1::DOF] = 0.5*(ncoords[:, 0]/length)
 # quadratic
-#u0[1::DOF] = 0.5*(ncoords[:, 0]/a)**2
+#u0[1::DOF] = 0.5*(ncoords[:, 0]/length)**2
 u0u = u0[bu]
 v0u = np.zeros_like(u0u)
 
@@ -97,7 +86,7 @@ plt.title('perturbation')
 plt.plot(ncoords[:, 0], u0[1::DOF])
 plt.show()
 
-nmodes = 50
+nmodes = 2
 print('wn', wn[:nmodes])
 
 c1 = []
@@ -105,8 +94,6 @@ c2 = []
 for I in range(nmodes):
     c1.append( V[:, I] @ L.T @ u0u )
     c2.append( V[:, I] @ L.T @ v0u / wn[I] )
-print('c1', c1)
-print('c2', c2)
 
 def ufunc(t):
     tmp = 0
@@ -123,6 +110,7 @@ u_xt = np.zeros((t.shape[0], K.shape[0]))
 u_xt[:, bu] = uu
 
 plt.ion()
+
 fig, axes = plt.subplots(nrows=2, figsize=(10, 5))
 for s in axes[0].spines.values():
     s.set_visible(False)
@@ -135,25 +123,24 @@ line = axes[1].plot([], [], '-')[0]
 line2 = axes[1].plot([], [], 'ro')[0]
 for i, ti in enumerate(t):
     ui = u_xt[i, :]
-    u1 = ui[0::DOF].reshape(nx, ny).T
-    u2 = ui[1::DOF].reshape(nx, ny).T
+    u1 = ui[0::DOF]
+    u2 = ui[1::DOF]
 
     axes[0].clear()
     axes[0].set_xticks([])
     axes[0].set_yticks([])
-    axes[0].set_xlim(0, 1.1*a)
+    axes[0].set_xlim(0, 1.1*length)
     axes[0].set_ylim(-2, 2)
     axes[0].set_title('t = %1.3f' % ti)
     xplot = xmesh + u1
     yplot = ymesh + u2
-    coords_plot = np.vstack((xplot.T.flatten(), yplot.T.flatten())).T
-    axes[0].triplot(coords_plot[:, 0], coords_plot[:, 1], d.simplices, lw=0.5)
-    axes[0].scatter(xplot[-1, -1], yplot[-1, -1], c='r')
+    axes[0].plot(xplot, yplot, 'k')
+    axes[0].scatter(xplot[-1], yplot[-1], c='r')
 
     axes[1].set_xlim(max(0, ti-0.5), ti+0.01)
-    data.append([ti, u2[-1, -1]])
+    data.append([ti, u2[-1]])
     line.set_data(np.asarray(data).T)
-    line2.set_data(ti, u2[-1, -1])
+    line2.set_data(ti, u2[-1])
 
     plt.pause(1e-15)
 
@@ -163,13 +150,13 @@ for i, ti in enumerate(t):
 
 # frequency analysis
 num = 100000
-t = np.linspace(0, 10, num)
+t = np.linspace(0, 1, num)
 uu = ufunc(t)
 u = np.zeros((t.shape[0], K.shape[0]))
 u[:, bu] = uu
 
 # get position of node corrsponding to the top right
-pos = np.where((ncoords[:, 0] == xmesh[-1, -1]) & (ncoords[:, 1] == ymesh[-1, -1]))[0][0]
+pos = np.where((ncoords[:, 0] == xmesh[-1]) & (ncoords[:, 1] == ymesh[-1]))[0][0]
 u1 = u[:, pos*DOF+1]
 
 plt.ioff()
@@ -181,7 +168,6 @@ plt.ylabel('$u_y$ top right')
 plt.xlabel('$t$')
 plt.show()
 
-
 # Fast Fourier Transform to get the frequencies
 uf = np.fft.fft(u1)
 dt = t[1]-t[0]
@@ -190,6 +176,6 @@ yf = 2/num*np.abs(uf[0:num//2])
 
 plt.xlabel('$rad/s$')
 plt.plot(xf, yf)
-plt.xlim(0, wn[4]+50)
+plt.xlim(0, wn[3]+50)
 plt.show()
 
