ba398e93818c76a5d5193ec98235584256a907b5
saullocastro <saullogiovani@gmail.com>
2020-02-12 14:13:19 +0100

    DEV: function to visualize sparse matrices

---
+++ tudaesasII/utils.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from scipy.sparse import coo_matrix
+
+def plot_sparse_matrix(m):
+    if not isinstance(m, coo_matrix):
+        m = coo_matrix(m)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, facecolor='black')
+    ax.plot(m.col, m.row, 's', color='white', ms=0.1)
+    ax.set_xlim(0, m.shape[1])
+    ax.set_ylim(0, m.shape[0])
+    ax.set_aspect('equal')
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+    ax.invert_yaxis()
+    ax.set_aspect('equal')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    return ax
