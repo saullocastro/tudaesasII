ae7087175e13e53d647263f2b8c4745338daf253
saullocastro <saullogiovani@gmail.com>
2022-02-11 16:48:19 +0100

    ENH: added initial static equilibrium, enhanced plotting

--- scripts_lectures/MDOF_systems/mdof0305_free_undamped_vibration.py
+++ scripts_lectures/MDOF_systems/mdof0305_free_undamped_vibration.py
@@ -5,17 +5,19 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.linalg import eigh, cholesky
+from scipy.linalg import eigh, cholesky, solve
 
 from tudaesasII.beam2d import Beam2D, update_K, update_M, DOF
 
+case = 2
+
 # number of nodes along x
-nx = 10
+nx = 11 #NOTE keep nx an odd number to have a node in the middle
 
 # geometry
-length = 10
-h = 1
-w = h/10
+length = 2
+h = 0.03
+w = h
 Izz = h**3*w/12
 A = w*h
 
@@ -49,18 +51,64 @@ for n1, n2 in zip(nids[:-1], nids[1:]):
     elem.rho = rho
     elem.interpolation = 'legendre'
     update_K(elem, nid_pos, ncoords, K)
-    update_M(elem, nid_pos, M)
+    update_M(elem, nid_pos, M, lumped=False)
     elems.append(elem)
 
-# applying boundary conditions
+# applying boundary conditions for the initial static equilibrium
 bk = np.zeros(K.shape[0], dtype=bool) # defining known DOFs
-check = np.isclose(x, 0.)
-bk[0::DOF] = check
-bk[1::DOF] = check
-bk[2::DOF] = check
+at_clamp = np.isclose(x, 0.)
+bk[0::DOF] = at_clamp
+bk[1::DOF] = at_clamp
+bk[2::DOF] = at_clamp
+if case == 1:
+    at_tip = np.isclose(x, length)
+    bk[1::DOF] += at_tip
+elif case == 2:
+    at_mid = np.isclose(x, length/2)
+    bk[1::DOF] += at_mid
+    at_tip = np.isclose(x, length)
+    bk[1::DOF] += at_tip
+else:
+    raise RuntimeError('Invalid case number')
+
 bu = ~bk # defining unknown DOFs
 
-# sub-matrices corresponding to unknown DOFs
+# partitioned matrices
+Kuu = K[bu, :][:, bu]
+Kuk = K[bu, :][:, bk]
+Kku = K[bk, :][:, bu]
+Kkk = K[bk, :][:, bk]
+Muu = M[bu, :][:, bu]
+
+# calculating static equilibrium
+u0 = np.zeros(K.shape[0])
+if case == 1:
+    u0[1::DOF][at_tip] = 0.05
+elif case == 2:
+    u0[1::DOF][at_mid] = -0.02
+    u0[1::DOF][at_tip] += 0.05
+
+uk = u0[bk] # initial displacement used to create the initial state
+
+fk = np.zeros(Kuu.shape[0]) # no external forces used to create the initial state
+uu = solve(Kuu, fk - Kuk@uk)
+fu = Kku@uu + Kkk@uk
+print('Force values to produce the prescribed displacements:')
+print(fu)
+
+u0[bu] = uu
+
+
+# boundary conditions for the dynamic problem
+bk = np.zeros(K.shape[0], dtype=bool) # defining known DOFs
+at_clamp = np.isclose(x, 0.)
+bk[0::DOF] = at_clamp
+bk[1::DOF] = at_clamp
+bk[2::DOF] = at_clamp
+
+bu = ~bk
+
+# partitioned matrices
 Kuu = K[bu, :][:, bu]
 Muu = M[bu, :][:, bu]
 
@@ -69,15 +117,10 @@ L = cholesky(Muu, lower=True)
 Linv = np.linalg.inv(L)
 Kuutilde = (Linv @ Kuu) @ Linv.T
 
-p = 10
-eigvals, V = eigh(Kuutilde, subset_by_index=(0, p-1))
+num_modes = 10
+eigvals, V = eigh(Kuutilde, subset_by_index=(0, num_modes-1))
 wn = eigvals**0.5
 
-u0 = np.zeros(K.shape[0])
-# linear
-u0[1::DOF] = 0.5*(ncoords[:, 0]/length)
-# quadratic
-#u0[1::DOF] = 0.5*(ncoords[:, 0]/length)**2
 u0u = u0[bu]
 v0u = np.zeros_like(u0u)
 
@@ -90,13 +133,13 @@ plt.show()
 
 c1 = []
 c2 = []
-for I in range(p):
+for I in range(num_modes):
     c1.append( V[:, I] @ L.T @ u0u )
     c2.append( V[:, I] @ L.T @ v0u / wn[I] )
 
 def ufunc(t):
     tmp = 0
-    for I in range(p):
+    for I in range(num_modes):
         tmp += (c1[I]*np.cos(wn[I]*t[:, None]) +
                 c2[I]*np.sin(wn[I]*t[:, None]))*(Linv.T@V[:, I])
     return tmp
@@ -116,6 +159,8 @@ for s in axes[0].spines.values():
 axes[1].spines['right'].set_visible(False)
 axes[1].spines['top'].set_visible(False)
 axes[1].set_ylim(u_xt[:, 1::DOF].min(), u_xt[:, 1::DOF].max())
+axes[1].set_xlabel('t')
+axes[1].set_ylabel('Tip displacement [m]')
 
 data = []
 line = axes[1].plot([], [], '-')[0]
@@ -128,8 +173,8 @@ for i, ti in enumerate(t):
     axes[0].clear()
     axes[0].set_xticks([])
     axes[0].set_yticks([])
-    axes[0].set_xlim(0, 1.1*length)
-    axes[0].set_ylim(-2, 2)
+    axes[0].set_xlim(0, 1.02*length)
+    axes[0].set_ylim(-1, 1)
     axes[0].set_title('t = %1.3f' % ti)
     xplot = xmesh + u1
     yplot = ymesh + u2
@@ -175,6 +220,6 @@ yf = 2/num*np.abs(uf[0:num//2])
 
 plt.xlabel('$rad/s$')
 plt.plot(xf, yf)
-plt.xlim(0, wn[p-1]+50)
+plt.xlim(0, wn[num_modes-1]+50)
 plt.show()
 
