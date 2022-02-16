ebb2b9ea2b4b21f8891bf55b8c1ad3d4346f6ecd
saullocastro <saullogiovani@gmail.com>
2022-02-16 12:36:26 +0100

    ENH: allowing figsize to be changed

--- tudaesasII/utils.py
+++ tudaesasII/utils.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 from scipy.sparse import coo_matrix
 
-def plot_sparse_matrix(m):
+def plot_sparse_matrix(m, figsize=(6, 6)):
     if not isinstance(m, coo_matrix):
         m = coo_matrix(m)
-    fig = plt.figure()
+    fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111, facecolor='black')
     ax.plot(m.col, m.row, 's', color='white', ms=0.1)
     ax.set_xlim(0, m.shape[1])
