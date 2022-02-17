fced060587884821c37b0008f079524fe65ccfc0
saullocastro <saullogiovani@gmail.com>
2022-02-17 14:52:14 +0100

    BUG: fixed modal force

--- scripts_lectures/MDOF_systems/mdof0309_displ_controlled_vibration_clamped.py
+++ scripts_lectures/MDOF_systems/mdof0309_displ_controlled_vibration_clamped.py
@@ -18,7 +18,7 @@ from tudaesasII.beam2d import Beam2D, update_K, update_M, DOF
 nx = 100
 
 # geometry
-Lbeam = 2
+length = 2
 h = 0.05
 w = h
 Izz = h**3*w/12
@@ -30,7 +30,7 @@ nu = 0.33
 rho = 2.6e3
 
 # creating mesh
-xmesh = np.linspace(0, Lbeam, nx)
+xmesh = np.linspace(0, length, nx)
 ymesh = np.zeros_like(xmesh)
 ncoords = np.vstack((xmesh.T.flatten(), ymesh.T.flatten())).T
 x = ncoords[:, 0]
@@ -53,7 +53,7 @@ for n1, n2 in zip(nids[:-1], nids[1:]):
     elem.A1 = elem.A2 = A
     elem.Izz1 = elem.Izz2 = Izz
     elem.rho = rho
-    elem.interpolation = 'legendre'
+    elem.interpolation = 'hermitian_cubic'
     update_K(elem, nid_pos, ncoords, K)
     update_M(elem, nid_pos, M, lumped=False)
     elems.append(elem)
@@ -86,19 +86,20 @@ Kuk = K[bu, :][:, bk]
 Kkk = K[bk, :][:, bk]
 
 L = cholesky(M, lower=True)
-Luu = L[bu, :][:, bu]
 Linv = np.linalg.inv(L)
+
+Luu = L[bu, :][:, bu]
 Linvuu = Linv[bu, :][:, bu]
 
 Ktildeuu = Linvuu @ Kuu @ Linvuu.T
 
-Nmodes = 12
-eigenvalues, Vu = eigh(Ktildeuu, subset_by_index=(0, Nmodes-1)) # already gives V[:, i] normalized to 1
+num_modes = 4
+eigenvalues, Vu = eigh(Ktildeuu, subset_by_index=(0, num_modes-1)) # already gives V[:, i] normalized to 1
 
 omegan = np.sqrt(eigenvalues)
 print('Natural frequencies [rad/s]', omegan)
 
-V = np.zeros((N, Nmodes))
+V = np.zeros((N, num_modes))
 V[bu, :] = Vu
 
 P = V
@@ -106,7 +107,7 @@ P = V
 # modal damping ratio
 zeta = 0.02
 # calculating damping matrix C from modal damping
-Dm = np.zeros((Nmodes, Nmodes))
+Dm = np.zeros((num_modes, num_modes))
 Dm[np.diag_indices_from(Dm)] = 2*zeta*omegan
 C = L @ P @ Dm @ P.T @ L.T
 Cuu = C[bu, :][:, bu]
@@ -114,19 +115,22 @@ Cuk = C[bu, :][:, bk]
 
 on = omegan
 od = on*np.sqrt(1 - zeta**2)
+print('Damped natural frequencies [rad/s]', od)
+
 #NOTE adding new np.array axis to vectorize calculations
 on = on[:, None]
 od = od[:, None]
 
 # base excitation
-tmax = 1
+tmax = 3.
 dt = 1e-3
 amplitude = 0.001
-omegaf = 60 #[rad/s]
+omegaf = omegan[0] #[rad/s]
 t = np.arange(0, tmax+dt, dt)
 u = np.zeros((N, t.shape[0]))
 udot = np.zeros((N, t.shape[0]))
 uddot = np.zeros((N, t.shape[0]))
+
 # prescribed displacement
 u[1,:] = amplitude*np.sin(omegaf*t)
 # prescribed velocity
@@ -141,22 +145,23 @@ udot0 = udot[:, 0]
 r0 = P.T @ L.T @ u0
 rdot0 = P.T @ L.T @ udot0
 
-uk = u[bk]
-udotk = udot[bk]
-uddotk = uddot[bk]
-
 # prescribed modal displacements
 r = P.T @ L.T @ u
 rdot = P.T @ L.T @ udot
 rddot = P.T @ L.T @ uddot
 
 # prescribed forces
-F = np.zeros((N, t.shape[0])) #NOTE no external forces applied
+F = np.zeros((N, t.shape[0]))
+#NOTE no external forces applied
 
 # forces from prescribed displacements, velocities and accelerations
+uk = u[bk]
+udotk = udot[bk]
+uddotk = uddot[bk]
 F[bu, :] += - Kuk @ uk - Cuk @ udotk - Muk @ uddotk
+
 # modal forces
-f = P.T @ Linv.T @ F
+f = P.T @ Linv @ F
 
 def r_t(t, t1, t2, on, zeta, od, fmodaln):
     """SDOF solution for a damped single impulse
@@ -165,7 +170,7 @@ def r_t(t, t1, t2, on, zeta, od, fmodaln):
     dt = t2 - t1
     H = np.heaviside(t - tn, 1.)
     h = 1/od*np.exp(-zeta*on*(t - tn))*np.sin(od*(t - tn))*H
-    return fmodaln*dt*h
+    return fmodaln[:, None]*dt*h
 
 # homogeneous solution for a damped SDOF system
 rh = np.exp(-zeta*on*t)*(r0[:, None]*np.cos(od*t) +
@@ -174,8 +179,13 @@ rh = np.exp(-zeta*on*t)*(r0[:, None]*np.cos(od*t) +
 # particular solution
 # discrete approximation of Duhamel's convolution integral
 rp = 0
-for t1, t2, f1, f2 in zip(t[:-1], t[1:], f[:-1], f[1:]):
-    rp += r_t(t, t1, t2, on, zeta, od, (f1+f2)/2)
+for i in range(len(t)-1):
+    t1 = t[i]
+    t2 = t[i+1]
+    f1 = f[:, i]
+    f2 = f[:, i+1]
+    fmodaln = (f1 + f2)/2
+    rp += r_t(t, t1, t2, on, zeta, od, fmodaln)
 
 # superposition between homogeneous and particular solutions
 r = rh + rp
@@ -183,23 +193,36 @@ r = rh + rp
 # transforming from modal space to physical space
 u[bu] = (Linv.T @ P @ r)[bu]
 
+#check = isclose(x, length)
+#plt.plot(t, 1000*u[1::DOF, :][check][0])
+#plt.show()
+#raise
 
+print('plotting...')
 # plotting animation
-fig = plt.figure(figsize=(12, 5))
+tplot = t[::10]
+uplot = u[:, ::10]
+
+fig = plt.figure(figsize=(12, 6))
 plt.xlabel('Beam length, [m]')
 plt.ylabel('Deflection [mm]')
-excitation = np.ones_like(x)*amplitude
 plt.plot(x, np.zeros_like(x), 'k--')
-line_excitation, = plt.plot(x[:1], u[1:2, 0], 'ro', ms=20)
-line_deflection, = plt.plot(x, u[1::DOF, 0], 'b-')
-plt.ylim(-0.01, 0.01)
+line_excitation, = plt.plot(x[:1], uplot[1:2, 0], 'ro', ms=20)
+line_deflection, = plt.plot(x, uplot[1::DOF, 0], 'b-')
 ax = plt.gca()
+plt.ylim(-40, 40)
+#plt.title('time = %1.2s')
 lines = ax.get_lines()
 
+m2mm = 1000
 def animate(i=0):
-    lines[1].set_ydata(u[1:2, i])
-    lines[2].set_ydata(u[1::DOF, i])
+    plt.title('$\omega_f=$%1.2f [rad/s]\n$t = $%1.2f [s]' % (omegaf, tplot[i]))
+    lines[1].set_ydata(uplot[1:2, i]*m2mm)
+    u_base = uplot[1, i]*m2mm
+    y_beam = u_base + uplot[1::DOF, i]*m2mm
+    y_beam[0] = u_base
+    lines[2].set_ydata(y_beam)
     return lines
 
-anim =FuncAnimation(fig, animate, range(len(t)))
-anim.save('mdof0309_movie.mp4', fps=20)
+anim = FuncAnimation(fig, animate, range(len(tplot)))
+anim.save('mdof0309_movie.mp4', fps=25)
