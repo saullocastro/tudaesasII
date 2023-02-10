e19366acb12088d812ce9753de564d9c943b242b
saullocastro <saullogiovani@gmail.com>
2023-02-10 16:54:12 +0100

    MAINT: updates for 2023

--- setup.py
+++ setup.py
@@ -80,7 +80,6 @@ Topic :: Software Development :: Libraries :: Python Modules
 Operating System :: Microsoft :: Windows
 Operating System :: Unix
 Operating System :: POSIX :: BSD
-Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
