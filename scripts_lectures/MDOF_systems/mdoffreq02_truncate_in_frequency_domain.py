686692603e4527c6c36c673a04cbefd5f1a8b586
Jurij Sodja <sodjaj@gmail.com>
2024-02-08 14:15:52 +0100

    Delete unused Jupyter notebooks and update code for frequency domain examples in MDOF

--- scripts_lectures/MDOF_systems/mdoffreq02_truncate_in_frequency_domain.py
+++ scripts_lectures/MDOF_systems/mdoffreq02_truncate_in_frequency_domain.py
@@ -34,12 +34,14 @@ f_t = f_t_func(t) # [N/kg]
 zeta = 0.02
 omegan = 10 * (2*pi) # [rad/s]
 
-plt.figure(figsize=(5, 3))
+size_figs = (10, 6)
+plt.figure(figsize=size_figs)
 plt.plot(t, f_t, 'o')
 plt.plot(t_plot, f_t_func(t_plot), 'b-', lw=1)
 plt.xlabel('Time, [$s$]')
 plt.ylabel('normalized force $f(t)$ [$N/kg$]')
 plt.xlim(0, 1)
+plt.tight_layout()
 plt.show()
 
 f_f = np.fft.rfft(f_t)[:Nsamples//2]/(Nsamples//2)
@@ -48,27 +50,30 @@ freqs_rad = freqs_Hz*(2*pi)
 if truncate:
     f_f[freqs_Hz > truncation_freq_Hz] = 0
 
-plt.figure(figsize=(5, 3))
-plt.stem(freqs_Hz, f_f.real, use_line_collection=True)
+plt.figure(figsize=size_figs)
+plt.stem(freqs_Hz, f_f.real)
 plt.xlim(0, 20)
 plt.xlabel('Frequency, [Hz]')
-plt.ylabel('normalized force $f(\omega)$ [$N/kg$]')
+plt.ylabel('normalized force $f(\\omega)$ [$N/kg$]')
+plt.tight_layout()
 plt.show()
 
 u_f = H(zeta, omegan, freqs_rad)*f_f
-plt.figure(figsize=(5, 3))
-plt.stem(freqs_Hz, u_f.real, use_line_collection=True)
+plt.figure(figsize=size_figs)
+plt.stem(freqs_Hz, u_f.real)
 plt.xlim(0, 20)
 plt.xlabel('Frequency, [Hz]')
-plt.ylabel('displacement $u(\omega)$ [$m$]')
+plt.ylabel('displacement $u(\\omega)$ [$m$]')
+plt.tight_layout()
 plt.show()
 
 u_t = np.fft.irfft(u_f*(Nsamples//2), n=Nsamples)
-plt.figure(figsize=(5, 3))
+plt.figure(figsize=size_figs)
 plt.plot(t, u_t, 'o', lw=0.5, mfc='None', mew=0.03)
 plt.xlim(0, 1)
 plt.ylim(-0.0001, 0.003)
 plt.xlabel('Time, [$s$]')
 plt.ylabel('displacement $u(t)$ [$m$]')
+plt.tight_layout()
 plt.show()
 
